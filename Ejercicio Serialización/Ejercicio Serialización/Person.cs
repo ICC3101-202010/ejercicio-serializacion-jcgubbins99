39ef08610ba6f5b54ecad02b3076c8c0b0a4b901
Juan Carlos <juancarlos.gubbins@gmail.com>
2020-05-07 10:27:50 -0400

    ejercicio

---
+++ Ejercicio Serialización/Ejercicio Serialización/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_Serialización
+{
+    [Serializable]
+    public class Person
+    {
+        private string Nombre;
+        private string Apellido;
+        private int Edad;
+
+        public string nombre
+        {
+            get
+            {
+                return Nombre;
+            }
+            set
+            {
+                Nombre = value;
+            }
+        }
+
+        public string apellido
+        {
+            get
+            {
+                return Apellido;
+            }
+            set
+            {
+                Apellido = value;
+                
+            }
+        }
+
+        public int edad
+        {
+            get
+            {
+                return Edad;
+            }
+            set
+            {
+                Edad = value;
+            }
+        }
+
+        public Person(string _nombre, string _apellido, int _edad)
+        {
+            this.nombre = _nombre;
+            this.apellido = _apellido;
+            this.edad = _edad;
+        }
+        public string information()
+        {
+            string informacion = ("Nombre: " + Nombre + "\n" + "Apellido: " + Apellido + "\n" + "Edad: " + Edad);
+            return informacion;
+        }
+    }
+}
---
+++ Ejercicio Serialización/Ejercicio Serialización/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace Ejercicio_Serialización
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            List<Person> listapersonas = new List<Person>();
+
+            string accion = null;
+            while (accion != "4")
+            {
+                Console.WriteLine("¿Qué desea hacer?");
+                Console.WriteLine("1. Almacenar");
+                Console.WriteLine("2. Cargar");
+                Console.WriteLine("3. Ver lista de usuarios");
+                Console.WriteLine("4. Salir");
+
+                accion = Console.ReadLine();
+                switch (accion)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("=====================");
+                        Console.WriteLine("Crear persona");
+                        Console.WriteLine("=====================");
+                        string nombre = "";
+                        string apellido = "";
+                        int edad = 0;
+                        Console.WriteLine("Ingrese su nombre ");
+                        nombre = Console.ReadLine();
+                        Console.WriteLine("Ingrese su apellido");
+                        apellido = Console.ReadLine();
+                        Console.WriteLine("Ingrese edad");
+                        edad = Convert.ToInt32(Console.ReadLine());
+                        Person p = new Person(nombre, apellido, edad);
+                        listapersonas.Add(p);
+                        Almacenar(listapersonas);
+                        break;
+                    case "2":
+                        Cargar(listapersonas);
+                        break;
+                    case "3":
+                        VerPersonas(listapersonas);
+                        break;
+                    case "4":
+                        Console.WriteLine("Has salido");
+                        break;
+                    default:
+                        Console.WriteLine("No has seleccionado ninguna opcion valida");
+                        break;
+                }
+            }
+
+
+            void Almacenar(List<Person> p)      //Serializamos
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("Usuarios.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, listapersonas);
+                stream.Close();
+
+            }
+            void Cargar(List<Person> p)
+            {
+                IFormatter formatter2 = new BinaryFormatter();
+                Stream stream = new FileStream("Usuarios.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                p = (List<Person>)formatter2.Deserialize(stream);
+                stream.Close();
+                VerPersonas(p);
+            }
+            void VerPersonas(List<Person> lista)
+            {
+                if (lista.Count == 0)
+                {
+                    Console.WriteLine("No hay personas agregadas aún");
+                }
+
+                else
+                {
+                    for (int i = 0; i < lista.Count; i++)
+                    {
+                        Console.WriteLine("============");
+                        Console.WriteLine("Persona" + " " + (i + 1));
+                        Console.WriteLine("============");
+                        Console.WriteLine(lista[i].information());
+                        Console.WriteLine(" ");
+
+                    }
+                }
+
+
+            }
+        }
+    }
+}
